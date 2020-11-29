9ea124690f5a7e46a5e1f916d3def76cba9c34db
Chow Li <chowli1103@gmail.com>
2020-11-29 01:22:01 -0800

    data imputation under dev

--- data_imputation/scripts/data_impute.py
+++ data_imputation/scripts/data_impute.py
@@ -7,4 +7,22 @@ import random
 class PostSalesReport:
 
     def __init__(self):
-        pass
+        self.vin = Vin()
+        self.incident = IncidentType()
+        self.vehicle = Vehicle()
+
+    def generate_records(self, n):
+        for _ in range(n):
+            print(self.vin.generate_vin())
+            print(self.incident.generate_incident())
+            print(self.vehicle.generate_vehicle_info())
+            print('-'*30)
+
+
+def main():
+    psr = PostSalesReport()
+    psr.generate_records(20)
+
+
+if __name__ == '__main__':
+    main()
