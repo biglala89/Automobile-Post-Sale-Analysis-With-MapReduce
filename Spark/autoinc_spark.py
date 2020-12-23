f7fe2daf2c8c7a74a62f96f2e24438b41bb7365b
Chow Li <chowli1103@gmail.com>
2020-12-23 02:59:10 -0800

    minor touch-ups

--- Spark/autoinc_spark.py
+++ Spark/autoinc_spark.py
@@ -1,23 +1,46 @@
 from pyspark import SparkContext
 import os
 
-sc = SparkContext()
 
-# Read the input data CSV file
-raw_rdd = sc.textFile(
-    '/Users/chowli/Documents/Springboard_DE/Projects/mini-projects/Post_Sale_Automobile_Report/source_data/data.csv')
-raw_rdd = raw_rdd.map(lambda x: x.split(','))
+def load_data():
+    sc = SparkContext()
+    data = sc.textFile('../source_data/data.csv')
+    return data
 
-vin_kv = raw_rdd.map(lambda x: (x[1], [x[0], x[2], x[4]]))
-vin_kv.cache()
 
-group_lvl_info = {}
-for kv in vin_kv.collect():
-    if kv[1][0] == 'I':
-        group_lvl_info[kv[0]] = kv[1][1:]
+def map_vin_pair():
+    """
+    Map raw daa to (vin, make, year) tuples.
+    """
+    raw_rdd = load_data()
+    raw_rdd = raw_rdd.map(lambda x: x.split(','))
+    vin_kv = raw_rdd.map(lambda x: (x[1], [x[0], x[2], x[4]]))
+    return vin_kv
+
+
+def cache_rdd(vin_kv):
+    """
+    Cache read dataset for faster future readings.
+    """
+    vin_kv.cache()
+
+
+def extract_group_master_info(vin_kv):
+    """
+    Extract make, year information from initial sale record per vin group.
+    """
+    group_lvl_info = {}
+    for kv in vin_kv.collect():
+        if kv[1][0] == 'I':
+            group_lvl_info[kv[0]] = kv[1][1:]
+    return group_lvl_info
 
 
 def populate_make(group, group_lvl_info):
+    """
+    Propagate group info to records with missing make and year 
+    and create new tuples.
+    """
     new_group = []
     vin = group[0]
     make_year_pair = list(group[1])
@@ -27,19 +50,45 @@ def populate_make(group, group_lvl_info):
     return new_group
 
 
-enhance_make = vin_kv.groupByKey().flatMap(
-    lambda x: populate_make(x, group_lvl_info))
+def populate_make_year(group_lvl_info, vin_kv):
+    """
+    Populate records for each vin group.
+    """
+    enhance_make = vin_kv.groupByKey().flatMap(
+        lambda x: populate_make(x, group_lvl_info))
+    return enhance_make
 
 
 def extract_make_key_value(pair):
     return ('-'.join(pair[1][1:]), 1)
 
 
-make_kv = enhance_make.map(lambda x: extract_make_key_value(x))
-incident_rdd = make_kv.reduceByKey(lambda a, b: a + b).sortByKey()
-incident_count = incident_rdd.collect()
-print(incident_count)
+def count_incidents(enhance_make):
+    """
+    Count the number of incidents per make and year combination.
+    """
+    make_kv = enhance_make.map(
+        lambda x: extract_make_key_value(x))
+    incident_rdd = make_kv.reduceByKey(lambda a, b: a + b).sortByKey()
+    incident_count = incident_rdd.collect()
+    print(incident_count)
+    return incident_count
 
-with open('reports.txt', 'w') as f:
-    for item in incident_count:
-        f.write("{},{}\n".format(item[0], str(item[1])))
+
+def save_output(incident_count):
+    with open('reports.txt', 'w') as f:
+        for item in incident_count:
+            f.write("{},{}\n".format(item[0], str(item[1])))
+
+
+def main():
+    vin_kv = map_vin_pair()
+    cache_rdd(vin_kv)
+    group_lvl_info = extract_group_master_info(vin_kv)
+    enhance_make = populate_make_year(group_lvl_info, vin_kv)
+    incident_count = count_incidents(enhance_make)
+    save_output(incident_count)
+
+
+if __name__ == '__main__':
+    main()
